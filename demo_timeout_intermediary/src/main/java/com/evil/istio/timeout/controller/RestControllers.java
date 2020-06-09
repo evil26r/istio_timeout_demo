3b45c8e5c58195438fe8b4aebd458c9fd6e28dc6
Evil <evil6821@gmail.com>
2020-06-09 16:37:49 +0300

    Add delay coefficient

--- demo_timeout_intermediary/src/main/java/com/evil/istio/timeout/controller/RestControllers.java
+++ demo_timeout_intermediary/src/main/java/com/evil/istio/timeout/controller/RestControllers.java
@@ -3,10 +3,7 @@ package com.evil.istio.timeout.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -23,13 +20,15 @@ public class RestControllers {
 
     private final WebClient webClient;
 
+    private double coefficient = 1;
+
     @RequestMapping
     public Mono<String> withDuration(@RequestParam(value = "durationMsIntermediary", defaultValue = "0") long durationMsIntermediary,
                                      @RequestHeader Map<String, String> headers) {
         log.info("Receive request. DurationMs: [{}]", durationMsIntermediary);
         log.info("Incoming headers: [{}]", headers);
         return Mono.just("Duration ms: " + durationMsIntermediary)
-                .delayElement(Duration.ofMillis(durationMsIntermediary));
+                .delayElement(Duration.ofMillis((long) (durationMsIntermediary * coefficient)));
     }
 
     @RequestMapping("chain")
@@ -49,8 +48,16 @@ public class RestControllers {
                 .retrieve()
                 .bodyToMono(String.class)
                 .doOnError(throwable -> log.warn("Error Request!", throwable))
-                .delayElement(Duration.ofMillis(durationMsIntermediary))
+                .delayElement(Duration.ofMillis((long) (durationMsIntermediary * coefficient)))
                 .map(response -> "DurationMsIntermediary: " + durationMsIntermediary + " " + response);
     }
 
+    @RequestMapping(value = {"coefficient", "coefficient/{coefficient}"})
+    public Mono<Double> setCoefficient(@PathVariable Double coefficient) {
+        if (coefficient != null) {
+            log.info("Incoming coefficient: [{}]", coefficient);
+            this.coefficient = coefficient;
+        }
+        return Mono.just(this.coefficient);
+    }
 }
